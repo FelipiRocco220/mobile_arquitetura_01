710011b716aca889b706b13353829558fac78580
Felipi Gabriel Soares Rocco <felipigabrielsoaresrocco@gmail.com>
2026-03-18 21:21:15 -0300

    Atividade 04b

---
+++ mobile_arquitetura_01-main/mobile_arquitetura_01-main/mobile_arquitetura_01/lib/presentation/product_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../domain/entities/product.dart';
+import '../../data/repositories/product_repository_impl.dart';
+import '../../data/datasources/product_remote_datasource.dart';
+
+class ProductProvider with ChangeNotifier {
+  List<Product> _products = [];
+  bool _isLoading = false;
+  String? _error;
+
+  List<Product> get products => _products;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+
+  final ProductRepositoryImpl _repository = ProductRepositoryImpl(ProductRemoteDataSource());
+
+  Future<void> fetchProducts() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _products = await _repository.getProducts();
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
